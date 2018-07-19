6778998ee33a59075a2b8db246816873c5b7bcab
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-07-19 13:48:43 +1200

    compute pfe

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    private static ArrayList<Integer> primeGenerator(Integer n) {
+        boolean[] primeNumbers = new boolean[n + 1];
+        ArrayList<Integer> result = new ArrayList<>();
+        Arrays.fill(primeNumbers, true);
+        for (int i = 2; i * i <= n; i++) {
+            if (!primeNumbers[i]) continue;
+            for (int mul = 2, b = i * mul; b <= n; mul++, b = i * mul) {
+                primeNumbers[b] = false;
+            }
+        }
+
+        for (int i = 2; i < primeNumbers.length; i++) {
+            if (primeNumbers[i]) result.add(i);
+        }
+        return result;
+    }
+
+    private static ArrayList<Integer> pfe(Integer n, ArrayList<Integer> primeNumbers) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (Integer i : primeNumbers) {
+            if (i != 1 && n % i == 0 && n / i != 1) {
+                result.add(i);
+                result.addAll(pfe(n / i, primeNumbers));
+                return result;
+            }
+        }
+        result.add(n);
+        return result;
+    }
+
+    private static void recursivePfe(Integer n, ArrayList<Integer> primeNumbers) {
+        System.out.println(pfe(n, primeNumbers));
+    }
+
+    public static void main(String[] args) {
+        Integer n = Integer.parseInt(args[0]);
+        ArrayList<Integer> primeNumbers = primeGenerator(n);
+        recursivePfe(n, primeNumbers);
+    }
+}
