34fda7d2d093693b2e38cd2218f1fb5e22c521ce
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-07-26 20:12:23 +1200

    update prob3 to use map

--- src/prob3.java
+++ src/prob3.java
@@ -1,32 +1,21 @@
 import java.util.*;
 
 public class prob3 {
-    private static String prettyPrint(List<Integer> uglyPrint) {
-        if (uglyPrint.size() == 0) return "";
-        String result;
-        int count = 0;
-        int prev = uglyPrint.get(0);
-        StringBuilder resultStringBuilder = new StringBuilder();
-        for (int i : uglyPrint) {
-            if (i == prev) {
-                count++;
-            } else if (count == 1) {
-                resultStringBuilder.append(Integer.toString(prev)).append("*");
-                prev = i;
-                count = 1;
+    private static String prettyPrint(Map<Integer, Integer> uglyPrint) {
+        StringBuilder result = new StringBuilder();
+        SortedSet<Integer> keys = new TreeSet<>(uglyPrint.keySet());
+        Iterator<Integer> keysIter = keys.iterator();
+        while (keysIter.hasNext()) {
+            Integer key = keysIter.next();
+            Integer value = uglyPrint.get(key);
+            if (value == 1) {
+                result.append(key);
             } else {
-                resultStringBuilder.append(Integer.toString(prev)).append("^").append(Integer.toString(count)).append("*");
-                prev = i;
-                count = 1;
+                result.append(key).append("^").append(value);
             }
+            if (keysIter.hasNext()) result.append("*");
         }
-        if (count == 1) {
-            resultStringBuilder.append(Integer.toString(prev));
-        } else {
-            resultStringBuilder.append(Integer.toString(prev)).append("^").append(Integer.toString(count));
-        }
-        result = resultStringBuilder.toString();
-        return result;
+        return result.toString();
     }
 
     public static void main(String[] args) {
@@ -34,27 +23,41 @@ public class prob3 {
         String[] input = reader.nextLine().trim().split("\\s+");
         reader.close();
         List<String> bucket = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> result = new TreeMap<>();
         boolean contain1 = false;
         for (String pfe : input) bucket.addAll(Arrays.asList(pfe.split("\\*")));
         for (String i : bucket) {
+            Integer key;
+            Integer oldValue;
+            Integer newValue;
             if (i.equals("1")) {
                 contain1 = true;
                 continue;
             }
             if (i.contains("^")) {
                 String[] temp = i.split("\\^");
-                for (int a = 0; a < Integer.parseInt(temp[1]); a++) {
-                    result.add(Integer.parseInt(temp[0]));
+                key = Integer.parseInt(temp[0]);
+                oldValue = result.get(key);
+                newValue = Integer.parseInt(temp[1]);
+                if (oldValue == null) {
+                    result.put(key, newValue);
+                } else {
+                    result.replace(key, oldValue + newValue);
                 }
+
             } else {
-                result.add(Integer.parseInt(i));
+                key = Integer.parseInt(i);
+                oldValue = result.get(key);
+                if (result.get(key) == null) {
+                    result.put(key, 1);
+                } else {
+                    result.replace(key, oldValue + 1);
+                }
             }
         }
         if (result.size() == 0 && contain1) {
             System.out.println("1");
         } else {
-            Collections.sort(result);
             System.out.println(prettyPrint(result));
         }
     }
